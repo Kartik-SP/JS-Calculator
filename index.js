acf19b16265a89756d1f1c3b246e15e771be7842
Kartik Singh Panwar <kartik2005dec@gmail.com>
2026-05-12 12:14:45 +0530

    HTMl Structure created

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let currentNum = "";
-let expression = "";
-let display = document.getElementById('output');
-function append(number){
-    currentNum += number;
-    expression += currentNum;
-    display.innerText = expression;
-}
-function show(){
-    display.innerHTML = eval(expression);
+function display(){
+    document.getElementById(`heading`).innerText = "JS is working!!";
 }
